0175f8cfed90e532f506c4b8c759328d39edb60d
Lucas <lucasmb.7@gmail.com>
2023-06-26 10:53:11 -0300

    Preparado para a prova (alunos adicionado)

--- src/Components/MUI/02CRUD/aluno/Cadastrar.jsx
+++ src/Components/MUI/02CRUD/aluno/Cadastrar.jsx
@@ -1,16 +1,51 @@
-import { Typography, TextField, Box, FormControl, InputLabel, Select, MenuItem } from "@mui/material"
+import { Typography, TextField, Box, Button, Container } from "@mui/material"
+import { useState } from "react"
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const Cadastrar = () => {
-    return(
-        <>
-            <Typography 
+
+    const [nome, setNome] = useState("");
+    const [curso, setCurso] = useState("");
+    const [ira, setIra] = useState("0.0");
+
+    const navigate = useNavigate()
+
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const novoAluno = {nome, curso, ira}
+        axios.post(
+            "http://localhost:3001/aluno/registrar",
+            novoAluno
+        )
+        .then((res)=>{
+            alert("Aluno ID " + res.data._id + " adicionado!")
+            navigate("/listarAluno")
+        })
+        .catch((err)=>console.log(err))
+    }
+
+    return (
+        <Container
+            sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+
+            }}
+        >
+            <Typography
                 variant="h5"
                 fontWeight="bold"
-            > 
+            >
                 Cadastrar Aluno
             </Typography>
-            <Box>
-                <TextField 
+            <Box
+                component="form"
+                onSubmit={handleSubmit}
+            >
+                <TextField
                     margin="normal"
                     required
                     fullWidth
@@ -18,31 +53,59 @@ const Cadastrar = () => {
                     id="nome"
                     name="nome"
                     label="Nome Completo"
+                    onChange={(event) => {
+                        setNome(event.target.value);
+                    }}
                 />
-                <TextField 
+
+                <TextField
                     margin="normal"
                     required
                     fullWidth
                     id="curso"
                     name="curso"
                     label="Curso"
+                    onChange={(event) => {
+                        setCurso(event.target.value);
+                    }}
                 />
-                <FormControl fullWidth sx={{mt: 2}}>
-                    <InputLabel id="select-tit-label">
-                        Titulação
-                    </InputLabel>
-                    <Select
-                        labelId="select-tit-label"
-                        label="Titulação"
-                    >
-                        <MenuItem value="GRAD">Graduação</MenuItem>
-                        <MenuItem value="MEST">Mestrado</MenuItem>
-                        <MenuItem value="DOUT">Doutorado</MenuItem>
 
-                    </Select>
-                </FormControl>
+                <TextField
+                    margin="normal"
+                    required
+                    fullWidth
+                    id="ira"
+                    name="ira"
+                    label="IRA"
+                    type="number"
+                    inputProps={{
+                        maxLength: 10,
+                        step: "0.1"
+                    }}
+                    onChange={(event) => {
+                        setIra(parseFloat(event.target.value));
+                    }}
+                />
+
+                <Box
+                    sx={{
+                        display: "flex",
+                        justifyContent: "center"
+                    }}
+                >
+                    <Button
+                        type="submit"
+                        variant="contained"
+                        sx={{
+                            my: 3
+                        }}
+                    >
+                        Cadastrar
+                    </Button>
+                </Box>
+
             </Box>
-        </>
+        </Container>
     )
 }
 
--- src/Components/MUI/02CRUD/aluno/Editar.jsx
+++ src/Components/MUI/02CRUD/aluno/Editar.jsx
@@ -1,8 +1,132 @@
+import { Typography, TextField, Box, Button, Container } from "@mui/material"
+import { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
+import axios from "axios"
+import { useNavigate } from "react-router-dom";
+
 const Editar = () => {
-    return(
-        <>
-            Editar Professor
-        </>
+
+    let {id} = useParams()
+    let navigate = useNavigate()
+
+    const [nome, setNome] = useState("");
+    const [curso, setCurso] = useState("");
+    const [ira, setIra] = useState("0.0");
+
+    useEffect(
+        () => {
+            axios.get(`http://localhost:3001/aluno/recuperar/${id}`)
+            .then(
+                (res) => {
+                    setNome(res.data.nome)
+                    setCurso(res.data.curso)
+                    setIra(res.data.ira)
+                }
+            )
+            .catch((err)=>console.log(err))
+        },
+        []
+    )
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        const alunoAtualizado = {nome, curso, ira};
+
+        axios.put(
+            `http://localhost:3001/aluno/atualizar/${id}`,
+            alunoAtualizado
+        )
+        .then((res)=>{
+            alert("Aluno ID " + id + " atualizado!")
+            navigate("/listarAluno")
+        })
+        .catch((err)=>console.log(err))
+        
+    }
+
+    return (
+        <Container
+            sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+
+            }}
+        >
+            <Typography
+                variant="h5"
+                fontWeight="bold"
+            >
+                Editar Aluno
+            </Typography>
+            <Box
+                component="form"
+                onSubmit={handleSubmit}
+            >
+                <TextField
+                    margin="normal"
+                    required
+                    fullWidth
+                    autoFocus
+                    id="nome"
+                    name="nome"
+                    label="Nome Completo"
+                    value={nome}
+                    onChange={(event) => {
+                        setNome(event.target.value);
+                    }}
+                />
+
+                <TextField
+                    margin="normal"
+                    required
+                    fullWidth
+                    id="curso"
+                    name="curso"
+                    label="Curso"
+                    value={curso}
+                    onChange={(event) => {
+                        setCurso(event.target.value);
+                    }}
+                />
+
+                <TextField
+                    margin="normal"
+                    required
+                    fullWidth
+                    id="ira"
+                    name="ira"
+                    label="IRA"
+                    type="number"
+                    inputProps={{
+                        maxLength: 10,
+                        step: "0.1"
+                    }}
+                    onChange={(event) => {
+                        setIra(parseFloat(event.target.value));
+                    }}
+                />
+
+                <Box
+                    sx={{
+                        display: "flex",
+                        justifyContent: "center"
+                    }}
+                >
+                    <Button
+                        type="submit"
+                        variant="contained"
+                        sx={{
+                            my: 3
+                        }}
+                    >
+                        Atualizar
+                    </Button>
+                </Box>
+
+            </Box>
+        </Container>
     )
 }
 
